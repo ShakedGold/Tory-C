064fc65a7d4814dd7153a486fc72b7022671e480
Shaked Gold <shakedgoldy@gmail.com>
2022-03-09 20:58:15 +0200

    Added Support For Windows, Mac & Linux

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,11 +16,16 @@ import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import net.bramp.ffmpeg.FFmpeg
 import net.bramp.ffmpeg.FFmpegExecutor
+import net.bramp.ffmpeg.FFmpegUtils
 import net.bramp.ffmpeg.FFprobe
 import net.bramp.ffmpeg.RunProcessFunction
 import net.bramp.ffmpeg.builder.FFmpegBuilder
+import net.bramp.ffmpeg.progress.Progress
+import net.bramp.ffmpeg.progress.ProgressListener
 import java.awt.FileDialog
 import java.io.File
+import java.util.concurrent.TimeUnit
+
 
 val func = RunProcessFunction()
 
@@ -35,7 +40,6 @@ fun App() {
 @Composable
 fun screen() {
     var files by remember { mutableStateOf(setOf<File>()) }
-
     Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
         Row {
             Text("Click On ", fontSize = 20.sp)
@@ -44,19 +48,51 @@ fun screen() {
         }
         Spacer(modifier = Modifier.padding(20.dp))
         Button(onClick = {
-            val ffmpeg = FFmpeg( "src/main/kotlin/FFmpeg Files/ffmpeg")
-            val ffprobe = FFprobe("src/main/kotlin/FFmpeg Files/ffprobe", func)
+            val os = OSValidator.checkOS()
+            var ffmpeg: FFmpeg? = null
+            var ffprobe: FFprobe? = null
+            when(os) {
+                "Windows" -> {
+                    ffmpeg = FFmpeg( "${System.getProperty("user.dir")}\\src\\main\\kotlin\\FFmpeg Files\\Windows\\ffmpeg")
+                    ffprobe = FFprobe("${System.getProperty("user.dir")}\\src\\main\\kotlin\\FFmpeg Files\\Windows\\ffprobe")
+                }
+                "Mac" -> {
+                    ffmpeg = FFmpeg( "${System.getProperty("user.dir")}src/main/kotlin/FFmpeg Files/Mac/ffmpeg")
+                    ffprobe = FFprobe("${System.getProperty("user.dir")}src/main/kotlin/FFmpeg Files/Mac/ffprobe")
+                }
+                "Linux" -> {
+                    ffmpeg = FFmpeg( "${System.getProperty("user.dir")}src/main/kotlin/FFmpeg Files/Linux/ffmpeg")
+                    ffprobe = FFprobe("${System.getProperty("user.dir")}src/main/kotlin/FFmpeg Files/Linux/ffprobe")
+                }
+                else -> {
+                    ffmpeg = FFmpeg( "${System.getProperty("user.dir")}\\src\\main\\kotlin\\FFmpeg Files\\Windows\\ffmpeg")
+                    ffprobe = FFprobe("${System.getProperty("user.dir")}\\src\\main\\kotlin\\FFmpeg Files\\Windows\\ffprobe")
+                }
+            }
             files = openFileDialog(ComposeWindow(), "Select File", listOf())
-
             files.forEach { file ->
-                val builder = FFmpegBuilder().setInput(file.path).addOutput("/Result/output.mp4").done()
-                val excecutor = FFmpegExecutor(ffmpeg, ffprobe)
-                excecutor.createJob(builder).run()
+                val executor = FFmpegExecutor(ffmpeg, ffprobe)
+                val `in` = ffprobe.probe(file.path)
+
+                val builder = FFmpegBuilder()
+                    .setInput(`in`)
+                    .addOutput("${file.name.split(".")[0]}.mp4")
+                    .done()
+                val job = executor.createJob(builder, object : ProgressListener {
+                    val duration_ns = `in`.getFormat().duration * TimeUnit.SECONDS.toNanos(1)
+                    override fun progress(progress: Progress) {
+                        val percentage = progress.out_time_ns / duration_ns
+                        println("${(percentage.toFloat() * 100).toInt()}% done")
+                    }
+                })
+                job.run()
             }
 
         }) { Text("Import Files") }
     }
 }
+
+
 fun openFileDialog(window: ComposeWindow, title: String, allowedExtensions: List<String>, allowMultiSelection: Boolean = true): Set<File> {
     return FileDialog(window, title, FileDialog.LOAD).apply {
         isMultipleMode = allowMultiSelection
@@ -74,7 +110,6 @@ fun openFileDialog(window: ComposeWindow, title: String, allowedExtensions: List
         isVisible = true
     }.files.toSet()
 }
-
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "File Convertor", state = WindowState(size = DpSize(430.dp, 400.dp), position = WindowPosition(Alignment.Center))) {
         App()
